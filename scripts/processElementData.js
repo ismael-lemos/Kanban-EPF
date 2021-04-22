df31e304d46f228464699da97828a7f5e3a6a556
ismaellemos <ismael.lemos.1997@gmail.com>
2021-04-21 22:19:58 -0300

    Push

---
+++ scripts/processElementData.js
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_kpfY8aL-EeuHK9s0arCvyQActivityDetail"]="./../../kanban/capabilitypatterns/resources/Gestão_98117C5B_98117c5b_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_MIU60aL_EeuHK9s0arCvyQActivityDetail"]="./../../kanban/capabilitypatterns/resources/Implementação_D1594D1A_d1594d1a_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_j1jlIaL_EeuHK9s0arCvyQActivityDetail"]="./../../kanban/capabilitypatterns/resources/Teste_749EB68D_749eb68d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQActivity"]="./../../kanban/deliveryprocesses/resources/Sprint_AACBDB4D_858be01b_Activity.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_hXi1IaMAEeuHK9s0arCvyQActivity"]="./../../kanban/deliveryprocesses/resources/Implementação_5ED5B45D_381e41cc_Activity.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_hXi1IaMAEeuHK9s0arCvyQActivityDetail"]="./../../kanban/deliveryprocesses/resources/Implementação_5ED5B45D_381e41cc_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_0NqxIaMBEeuHK9s0arCvyQActivity"]="./../../kanban/deliveryprocesses/resources/Teste_14C6844B_ee0f11ba_Activity.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_0NqxIaMBEeuHK9s0arCvyQActivityDetail"]="./../../kanban/deliveryprocesses/resources/Teste_14C6844B_ee0f11ba_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQActivity"]="./../../kanban/deliveryprocesses/resources/Processo_7682C47A_7682c47a_Activity.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_f60YcaMAEeuHK9s0arCvyQActivity"]="./../../kanban/deliveryprocesses/resources/Gestão_2BE12624_529b393_Activity.jpeg"
+contentPage.processPage.imageFiles["_aNm-AaMAEeuHK9s0arCvyQ,_c5UWUaMAEeuHK9s0arCvyQ,_f60YcaMAEeuHK9s0arCvyQActivityDetail"]="./../../kanban/deliveryprocesses/resources/Gestão_2BE12624_529b393_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_2enncaL-EeuHK9s0arCvyQ"] = ["Gerente","kanban/roles/gerente_33FC1C62.html"];
+contentPage.processPage.elementUrls["_c5UWUaMAEeuHK9s0arCvyQ"] = ["Sprint","kanban/deliveryprocesses/Sprint_AACBDB4D.html"];
+contentPage.processPage.elementUrls["_XTNe4KL_EeuHK9s0arCvyQ"] = ["Realizar tarefa","kanban/tasks/realizar_tarefa_23EDF789.html"];
+contentPage.processPage.elementUrls["_le0pAaL_EeuHK9s0arCvyQ"] = ["Tester","kanban/roles/tester_1081F4F.html"];
+contentPage.processPage.elementUrls["_le0pA6L_EeuHK9s0arCvyQ"] = ["Gerente","kanban/roles/gerente_33FC1C62.html"];
+contentPage.processPage.elementUrls["_MIU60aL_EeuHK9s0arCvyQ"] = ["Implementação","kanban/capabilitypatterns/Implementação_D1594D1A.html"];
+contentPage.processPage.elementUrls["_f60YcaMAEeuHK9s0arCvyQ"] = ["Gestão","kanban/deliveryprocesses/Gestão_2BE12624.html"];
+contentPage.processPage.elementUrls["_XTNe46L_EeuHK9s0arCvyQ"] = ["Gerente","kanban/roles/gerente_33FC1C62.html"];
+contentPage.processPage.elementUrls["_XTNe5aL_EeuHK9s0arCvyQ"] = ["Cartão","kanban/workproducts/cartao_E0F4B6FE.html"];
+contentPage.processPage.elementUrls["_2enneaL-EeuHK9s0arCvyQ"] = ["Quadro","kanban/workproducts/quadro_3102A26F.html"];
+contentPage.processPage.elementUrls["_gRaI8KL_EeuHK9s0arCvyQ"] = ["Implantação","kanban/tasks/implantao_E629F3E9.html"];
+contentPage.processPage.elementUrls["_j1jlIaL_EeuHK9s0arCvyQ"] = ["Teste","kanban/capabilitypatterns/Teste_749EB68D.html"];
+contentPage.processPage.elementUrls["_0NqxIaMBEeuHK9s0arCvyQ"] = ["Teste","kanban/deliveryprocesses/Teste_14C6844B.html"];
+contentPage.processPage.elementUrls["_le0pBaL_EeuHK9s0arCvyQ"] = ["Cartão","kanban/workproducts/cartao_E0F4B6FE.html"];
+contentPage.processPage.elementUrls["_hXi1IaMAEeuHK9s0arCvyQ"] = ["Implementação","kanban/deliveryprocesses/Implementação_5ED5B45D.html"];
+contentPage.processPage.elementUrls["_2enndaL-EeuHK9s0arCvyQ"] = ["Cartão","kanban/workproducts/cartao_E0F4B6FE.html"];
+contentPage.processPage.elementUrls["_2enncKL-EeuHK9s0arCvyQ"] = ["Definir tarefa","kanban/tasks/definir_tarefa_5B3032E0.html"];
+contentPage.processPage.elementUrls["_aNm-AaMAEeuHK9s0arCvyQ"] = ["Processo","kanban/deliveryprocesses/Processo_7682C47A.html"];
+contentPage.processPage.elementUrls["_XTNe4aL_EeuHK9s0arCvyQ"] = ["Desenvolvedor","kanban/roles/desenvolvedor_C571CA1E.html"];
+contentPage.processPage.elementUrls["_le0pAKL_EeuHK9s0arCvyQ"] = ["Definir casos de teste","kanban/tasks/definir_casos_de_teste_17E70238.html"];
+contentPage.processPage.elementUrls["_XTNe56L_EeuHK9s0arCvyQ"] = ["Colunas","kanban/workproducts/colunas_EAFAAC2C.html"];
+contentPage.processPage.elementUrls["_XTNe6aL_EeuHK9s0arCvyQ"] = ["Quadro","kanban/workproducts/quadro_3102A26F.html"];
+contentPage.processPage.elementUrls["_m7Z7wKL_EeuHK9s0arCvyQ"] = ["Realizar teste","kanban/tasks/realizar_teste_AB2C69DB.html"];
+contentPage.processPage.elementUrls["_eCDVcKL_EeuHK9s0arCvyQ"] = ["Resalizar testes unitários","kanban/tasks/resalizar_testes_unitrios_A25FB43E.html"];
+contentPage.processPage.elementUrls["_2ennc6L-EeuHK9s0arCvyQ"] = ["Desenvolvedor","kanban/roles/desenvolvedor_C571CA1E.html"];
+contentPage.processPage.elementUrls["_le0pB6L_EeuHK9s0arCvyQ"] = ["Colunas","kanban/workproducts/colunas_EAFAAC2C.html"];
+contentPage.processPage.elementUrls["_2ennd6L-EeuHK9s0arCvyQ"] = ["Colunas","kanban/workproducts/colunas_EAFAAC2C.html"];
+contentPage.processPage.elementUrls["_30SZ4KL-EeuHK9s0arCvyQ"] = ["Definir metas","kanban/tasks/definir_metas_8667316F.html"];
+contentPage.processPage.elementUrls["_kpfY8aL-EeuHK9s0arCvyQ"] = ["Gestão","kanban/capabilitypatterns/Gestão_98117C5B.html"];
+contentPage.processPage.elementUrls["_le0pCaL_EeuHK9s0arCvyQ"] = ["Quadro","kanban/workproducts/quadro_3102A26F.html"];
